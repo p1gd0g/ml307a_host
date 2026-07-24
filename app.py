aafe466b35148053296af635ee18d1fc103f5b7d
p1gd0g <renhao.szu@gmail.com>
2026-07-25 00:14:49 +0800

    test: 更新 webhook 测试用例的短信内容

--- app.py
+++ app.py
@@ -197,7 +197,7 @@ async def test_webhook(request: Request):
 
     test_msg = {
         "sender": "13800138000",
-        "content": "test",
+        "content": "测试",
         "timestamp": "",
     }
 
